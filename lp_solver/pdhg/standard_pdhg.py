f6c25232437455c822b87f131dc9706e191fdf65
morphism42 <sherlockdace@outlook.com>
2025-04-03 23:06:31 +0800

    Add requirements.txt and refactor PDHG solver implementation
    - Introduced a new requirements.txt file with necessary dependencies.
    - Refactored the PDHG solver by replacing the PDHGSolver class with a standalone function `standard_pdhg_solve` for improved usability.
    - Updated test.py to reflect changes in the solver implementation and adjusted matrix dimensions for testing.

--- lp_solver/pdhg/standard_pdhg.py
+++ lp_solver/pdhg/standard_pdhg.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
-from typing import NamedTuple, Optional
+from typing import Optional
 
 import chex
-import jax
 import jax.numpy as jnp
 from jax import lax
 
@@ -19,70 +18,96 @@ class PDHGState(StandardLPState):
     tol: float
 
 
-@dataclass
-class PDHGSolver:
-    """PDHG solver for linear programming problems"""
+def standard_pdhg_solve(
+    vec_c: jnp.ndarray,
+    mat_A: jnp.ndarray,
+    vec_b: jnp.ndarray,
+    vec_x_init: Optional[jnp.ndarray] = None,
+    vec_y_init: Optional[jnp.ndarray] = None,
+    eta: Optional[float] = None,
+    omega: Optional[float] = None,
+    max_iter: int = 5000,
+    tol: float = 1e-4,
+    dtype: jnp.dtype = jnp.float32,
+) -> PDHGState:
+    r"""Solve the standardlinear programming problem using the PDHG algorithm.
+    
+    The standard linear programming problem is of the form:
+    min c^T x
+    s.t. Ax = b
+    x >= 0
+    
+    The PDHG algorithm is a primal-dual method for solving this problem.
+    The algorithm is described in many papers, for example:https://arxiv.org/abs/2106.04756
+    
+    Args:
+        `vec_c`: The cost vector of the linear programming problem.
+        `mat_A`: The constraint matrix of the linear programming problem.
+        `vec_b`: The right-hand side vector of the linear programming problem.
+        `vec_x_init`: The initial guess for the primal variable.
+        `vec_y_init`: The initial guess for the dual variable.
+        `eta`: The step size for the primal variable.
+        `omega`: The step size for the dual variable.
+        `max_iter`: The maximum number of iterations.
+        `tol`: The tolerance for the stopping criterion.
+        `dtype`: The data type of the variables.
 
-    def __init__(
-        self,
-        max_iter: int = 5000,
-        tol: float = 1e-4,
-        dtype: jnp.dtype = jnp.float32,
-    ):
-        self.dtype = dtype
-        self.max_iter = max_iter
-        self.tol = tol
+    Returns:
+        `PDHGState`: The final state of the PDHG algorithm.
+    """
+    init_state = init_state_genearte(
+        vec_c=vec_c,
+        mat_A=mat_A,
+        vec_b=vec_b,
+        vec_x_init=vec_x_init,
+        vec_y_init=vec_y_init,
+        eta=eta,
+        omega=omega,
+        dtype=dtype,
+        max_iter=max_iter,
+        tol=tol,
+    )
+    final_state = lax.while_loop(_cond_fn, _update_fn, init_state)
+    return final_state
 
-    def solve(
-        self,
-        vec_c: jnp.ndarray,
-        mat_A: jnp.ndarray,
-        vec_b: jnp.ndarray,
-        vec_x_init: Optional[jnp.ndarray] = None,
-        vec_y_init: Optional[jnp.ndarray] = None,
-        eta: Optional[float] = None,
-        omega: Optional[float] = None,
-    ) -> PDHGState:
-        """Solve the linear programming problem using the PDHG algorithm."""
-        state = self.init_state(vec_c, mat_A, vec_b, vec_x_init, vec_y_init, eta, omega)
-        state = lax.while_loop(_cond_fn, _update_fn, state)
-        return state
 
-    def init_state(
-        self,
-        vec_c: jnp.ndarray,
-        mat_A: jnp.ndarray,
-        vec_b: jnp.ndarray,
-        vec_x_init: Optional[jnp.ndarray] = None,
-        vec_y_init: Optional[jnp.ndarray] = None,
-        eta: Optional[float] = None,
-        omega: Optional[float] = None,
-    ) -> PDHGState:
-        # Initialize primal and dual variables
-        if vec_x_init is None:
-            vec_x_init = jnp.zeros(mat_A.shape[1], dtype=self.dtype)
-        if vec_y_init is None:
-            vec_y_init = jnp.zeros(mat_A.shape[0], dtype=self.dtype)
+def init_state_genearte(
+    vec_c: jnp.ndarray,
+    mat_A: jnp.ndarray,
+    vec_b: jnp.ndarray,
+    vec_x_init: Optional[jnp.ndarray] = None,
+    vec_y_init: Optional[jnp.ndarray] = None,
+    eta: Optional[float] = None,
+    omega: Optional[float] = None,
+    dtype: jnp.dtype = jnp.float32,
+    max_iter: int = 5000,
+    tol: float = 1e-4,
+) -> PDHGState:
+    # Initialize primal and dual variables
+    if vec_x_init is None:
+        vec_x_init = jnp.zeros(mat_A.shape[1], dtype=dtype)
+    if vec_y_init is None:
+        vec_y_init = jnp.zeros(mat_A.shape[0], dtype=dtype)
 
-        if eta is None:
-            eta = 0.9 / jnp.linalg.norm(mat_A, ord=2)
-        if omega is None:
-            omega = 1.0
+    if eta is None:
+        eta = 0.9 / (jnp.linalg.norm(mat_A, ord=2) + 1e-10)
+    if omega is None:
+        omega = 1.0
 
-        # Initialize state
-        state = PDHGState(
-            vec_c=jnp.asarray(vec_c, dtype=self.dtype),
-            mat_A=jnp.asarray(mat_A, dtype=self.dtype),
-            vec_b=jnp.asarray(vec_b, dtype=self.dtype),
-            vec_x=vec_x_init,
-            vec_y=vec_y_init,
-            eta=eta,
-            omega=omega,
-            curr_iter=0,
-            max_iter=self.max_iter,
-            tol=self.tol,
-        )
-        return state
+    # Initialize state
+    state = PDHGState(
+        vec_c=jnp.asarray(vec_c, dtype=dtype),
+        mat_A=jnp.asarray(mat_A, dtype=dtype),
+        vec_b=jnp.asarray(vec_b, dtype=dtype),
+        vec_x=vec_x_init,
+        vec_y=vec_y_init,
+        eta=eta,
+        omega=omega,
+        curr_iter=0,
+        max_iter=max_iter,
+        tol=tol,
+    )
+    return state
 
 
 def _cond_fn(pdhg_state: PDHGState) -> bool:
--- test.py
+++ test.py
@@ -1,32 +1,33 @@
 from time import time
 
-import jax 
+import jax
 import jax.numpy as jnp
 import scipy
 import scipy.optimize
 
-from lp_solver.pdhg.standard_pdhg import PDHGSolver
+from lp_solver.pdhg.standard_pdhg import standard_pdhg_solve
 
 
 jax.config.update("jax_enable_x64", True)
 
-m = 50
-n = 200
+m = 100
+n = 500
 mat_A = jax.random.normal(jax.random.PRNGKey(1), (m, n))
 vec_b = jax.random.normal(jax.random.PRNGKey(1), (m,))
 vec_c = jax.random.normal(jax.random.PRNGKey(1), (n,)) / n + 100
 
-solver = PDHGSolver(max_iter=50000, tol=1e-4, dtype=jnp.float64)
-
 tic = time()
-out = solver.solve(
+state = standard_pdhg_solve(
     vec_c=vec_c,
     mat_A=mat_A,
     vec_b=vec_b,
+    dtype=jnp.float64,
+    tol=1e-4,
+    max_iter=50000,
 )
 toc = time()
 print(f"Time taken: {toc - tic:.4f} seconds")
-print(f"The optimal value is: {jnp.vdot(out.vec_c, out.vec_x)}")
+print(f"The optimal value is: {jnp.vdot(state.vec_c, state.vec_x)}")
 
 tic = time()
 out2 = scipy.optimize.linprog(
